806f8a26bd0b3c88cceefe105506a309bb410741
John Ochs <code@johnochs.io>
2023-06-03 02:31:14 -0700

    Implements most of the initialization functionality of the UNA1 class

--- lib/turing_machine.rb
+++ lib/turing_machine.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "turing_machine/version"
-require_relative "turing_machine/una1"
-
 
 module TuringMachine
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require "turing_machine/una1"
--- lib/turing_machine/una1.rb
+++ lib/turing_machine/una1.rb
@@ -1,4 +1,23 @@
+# frozen_string_literal: true
+
 module TuringMachine
-    class UNA1
+  class UNA1
+    INSTRUCTIONS = [].freeze
+
+    def self.validate_tape_sequence(tape_sequence)
+      if tape_sequence.nil? || tape_sequence.empty?
+        raise ArgumentError, "'tape_sequence' argument cannot be nil or empty."
+      end
+
+      clean_seq = tape_sequence.map!(&:to_i)
+      raise ArgumentError, "'tape_sequence' argument is too short (length = #{clean_seq.size})." if clean_seq.size < 3
+      raise ArgumentError, "Invalid 'tape_sequence' argument!" unless clean_seq.join.match?(/^0{1,}1{1,}0{1,}$/)
+
+      @tape_sequence = clean_seq
     end
+
+    def initialize(tape_sequence = nil)
+      @tape_sequence = UNA1.validate_tape_sequence(tape_sequence)
+    end
+  end
 end
